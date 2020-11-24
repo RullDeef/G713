d91d4b15db6f93f52fd8785a72a850d8bfcfbb98
Rull Deef <deeroll666@gmail.com>
2020-11-24 07:50:21 +0000

    add canvas

--- src/canvas.js
+++ src/canvas.js
@@ -15,17 +15,3 @@ const Canvas = {
         resize()
     }
 }
-
-window.addEventListener("load", () => {
-    Canvas.init()
-
-    Canvas.ctx.strokeRect(10, 10, 200, 100)
-
-    console.log("hello, world!")
-})
-class User {
-    constructor() {
-        this.name = "Guest"
-        this.age = 18
-    }
-}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 
 window.addEventListener("load", () => {
+    Canvas.init()
+
+    Canvas.ctx.strokeRect(10, 10, 200, 100)
+
     console.log("hello, world!")
 })
